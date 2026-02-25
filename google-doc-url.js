cd7244e494ab50cbfab9097e91162c2c898337f4
Mariana-cazp <mcastroa7@gmail.com>
2026-02-25 14:40:21 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,4 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xVPSqchowNpkb_QpNcpeLQy6kEiTlavCcF41tfsiLo4/edit?gid=1033046795#gid=1033046795';
-
+var googleDocURL = null;
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
